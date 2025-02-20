20c893f1a26b1d637b4f64306ad395f4af428cb2
Bor1sss <78493730+Bor1sss@users.noreply.github.com>
2025-02-20 19:27:06 +0200

    Dz Calc

--- app/src/main/java/com/example/android_213/MainActivity.java
+++ app/src/main/java/com/example/android_213/MainActivity.java
@@ -1,53 +1,20 @@
 package com.example.android_213;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
-
-//import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-//import androidx.core.graphics.Insets;
-//import androidx.core.view.ViewCompat;
-//import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    //    private TextView tv1;
-    private TextView tv2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-//        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-//            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-//            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-//            return insets;
-//        });
-
-        tv2 = findViewById(R.id.textView2);
-        findViewById(R.id.buttonPlus).setOnClickListener(this::onButtonPlusClick);
-        findViewById(R.id.buttonMinus).setOnClickListener(this::onButtonMinusClick);
-//        tv1 = findViewById(R.id.textView1);
-//        findViewById(R.id.button1).setOnClickListener(this::onButton1Click);
+        findViewById(R.id.main_btn_calc).setOnClickListener(this::onButtonCalc);
     }
 
-//    private void onButton1Click(View view) {
-//
-////        String txt = tv1.getText().toString();
-////        txt += "!";
-////        tv1.setText(txt);
-//    }
-
-    private void onButtonPlusClick(View view) {
-        int number = Integer.parseInt(tv2.getText().toString());
-        number += 1;
-        tv2.setText(String.valueOf(number));
-    }
-
-    private void onButtonMinusClick(View view) {
-        int number = Integer.parseInt(tv2.getText().toString());
-        number -= 1;
-        tv2.setText(String.valueOf(number));
+    private void onButtonCalc(View view) {
+        startActivity(new Intent(MainActivity.this, CalcActivity.class));
     }
 }
